62ec1bac98c8dae64c584b8044ebd0fef137fe4a
Soham Mukherjee <sohamantipr000@gmail.com>
2021-11-14 22:00:22 +0530

    Added viewsets for our Todoapp

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("user", views.UserViewSet)
+router.register("todo", views.Todo)
 
 urlpatterns = [
     path("", views.hello, name="hello")
 ]
+
+urlpatterns += router.urls
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,22 @@
 from django.http import JsonResponse
+from rest_framework.viewsets import ModelViewSet
+from .models import User, Todo
+from .serializers import UserSerializer, TodoSerializer
 
 
 def hello(request):
     return JsonResponse({
         "message": "Hello welcome to todoapp"
     }, status=200)
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class Todo(ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
