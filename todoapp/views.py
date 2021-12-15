6058622600a4ca7b8a2bddc37a9e532ef1ebe6ee
Soham Mukherjee <sohamantipr000@gmail.com>
2021-12-15 20:54:48 +0530

    Added dockerfile

--- todoapp/views.py
+++ todoapp/views.py
@@ -10,7 +10,7 @@ import os
 def hello(request):
     env = os.environ.get("ENV", "None")
     return JsonResponse({
-        "message": "Hello world. Environment: " + env
+        "message": "Hi we are learning bind mounts. Environment: " + env
     }, status=200)
 
 
