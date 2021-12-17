b6e5f041751787ed7309d208d04cca73b6311eff
Soham Mukherjee <sohamantipr000@gmail.com>
2021-12-17 20:47:37 +0530

    Added celery and redis with our project

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -132,5 +132,5 @@ CELERY_RESULT_BACKEND = 'redis://' + os.environ.get("REDIS_ENDPOINT") + ':6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Mumbai'
+CELERY_TIMEZONE = 'Asia/Dhaka'
 # redis://localhost:6379
