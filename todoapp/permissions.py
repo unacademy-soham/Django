741d33165e7126f9705086d8907ee5e323bd5df7
Soham Mukherjee <sohamantipr000@gmail.com>
2021-12-29 19:46:00 +0530

    Added celery and redis with our project

--- todoapp/permissions.py
+++ todoapp/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework.permissions import BasePermission
+from .models import Users
 
 
 class AdminPermissions(BasePermission):
 
     def has_permission(self, request, view):
+        user = Users.objects.get_by_natural_key(username=request.user.username)
         if request.method in ["POST", "PATCH", "DELETE"]:
-            return request.user.is_admin
+            return user.is_admin
         return True
 
     def has_object_permission(self, request, view, obj):
@@ -13,7 +15,7 @@ class AdminPermissions(BasePermission):
         # For cart user attribute is not available
         if request.method in ["PATCH", "DELETE"]:
             if obj.__class__.__name__ == "Shop":
-                if obj.user == request.user:
+                if obj.user.username == request.user.username:
                     return True
             else:
                 return False
