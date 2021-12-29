7a4774591830981271fa6e3ab24e15322290086d
Soham Mukherjee <sohamantipr000@gmail.com>
2021-12-29 20:16:02 +0530

    Added celery and redis with our project

--- todoapp/permissions.py
+++ todoapp/permissions.py
@@ -18,7 +18,9 @@ class AdminPermissions(BasePermission):
         # For cart user attribute is not available
         print(request.method in ["PATCH", "DELETE"])
         print(request.user.username)
-        print(type(obj))
+        print(type(obj) == type(Shops))
+        print(type(Shops))
+        print(obj._meta)
         print(obj.user.username == request.user.username)
         if request.method in ["PATCH", "DELETE"]:
             if type(obj) == type(Shops):
